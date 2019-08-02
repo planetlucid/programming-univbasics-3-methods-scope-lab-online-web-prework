da5ebe7a024a06c9c91ea9f21e36a26bb19ca6de
restful-strategy-4685 <planetlucid@gmail.com>
2019-08-02 17:52:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,11 @@ def mario
 end
 
 def toadstool
-  status = 
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link 
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase
 end
